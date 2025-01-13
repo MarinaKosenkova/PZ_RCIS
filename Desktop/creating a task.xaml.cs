fdcc4a222ea234935a8cde8c98df7e83ba930a63
MarinaKosenkova <koff.06@mail.ru>
2025-01-13 21:44:04 +0300

    Add files via upload

---
+++ Desktop/creating a task.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Логика взаимодействия для creating_a_task.xaml
+    /// </summary>
+    public partial class creating_a_task : Window
+    {
+        public creating_a_task()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Main_empty thirdWindow = new Main_empty();
+            thirdWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            master sixWindow = new master();
+            sixWindow.Show();
+            this.Close();
+        }
+    }
+}
