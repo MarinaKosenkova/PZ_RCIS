f8d5cf0b9f2ab81fe563ebb936f62e433c0b29e1
MarinaKosenkova <koff.06@mail.ru>
2025-01-13 21:38:59 +0300

    Add files via upload

---
+++ UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Todo.Entities
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public UserModel(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
