12967f182020192d7e10e9b1c48cf354c5d55c3f
MarinaKosenkova <koff.06@mail.ru>
2025-02-10 18:50:20 +0300

    Add files via upload

---
+++ View/Main.xaml.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Desktop.Repository;
+using Desktop.TaskFolder;
+using Entities;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Animation;
+
+namespace Desktop.View
+{
+    public partial class Main : Page
+    {
+        private bool showCompletedTasks = false;
+        private List<TaskDictionary> userTasks;
+        private string userName;
+        private string selectedCategory = "All";
+
+        public Main(string name, List<TaskDictionary> tasks)
+        {
+            InitializeComponent();
+            userName = name;
+            UserNameBlock.Text = name;
+            userTasks = tasks;
+            InitializeTasks(userTasks);
+            InitializeCategories();
+
+            TaskRepository.GetTaskRepository().TaskItemsChanged += RefreshTasks;
+
+            // Animation for UserNameBlock
+            var animation = new DoubleAnimation
+            {
+                From = 0.0,
+                To = 1.0,
+                Duration = new Duration(TimeSpan.FromSeconds(1))
+            };
+            UserNameBlock.BeginAnimation(UIElement.OpacityProperty, animation);
+        }
+
+        private void InitializeCategories()
+        {
+            var categories = userTasks.Select(t => t.Category).Distinct().ToList();
+            foreach (var category in categories)
+            {
+                if (!CategorySPanel.Children.Cast<TextBlock>().Any(tb => tb.Text == category))
+                {
+                    var textBlock = new TextBlock
+                    {
+                        Text = category,
+                        FontSize = 18,
+                        Margin = new Thickness(10, 0, 0, 0),
+                        Foreground = Brushes.Black,
+                        FontWeight = FontWeights.DemiBold
+                    };
+                    textBlock.MouseDown += CategoryTextBlock_MouseDown;
+                    CategorySPanel.Children.Add(textBlock);
+                }
+            }
+        }
+
+        private void InitializeTasks(List<TaskDictionary> taskItems)
+        {
+            ItemsTasksSPanel.Children.Clear();
+
+            foreach (TaskDictionary task in taskItems)
+            {
+                if ((showCompletedTasks && task.IsCompleted) || (!showCompletedTasks && !task.IsCompleted && (selectedCategory == "All" || task.Category == selectedCategory)))
+                {
+                    var item = new TaskItem();
+                    item.InfoLoad(task);
+                    item.Margin = new Thickness(10);
+                    item.Effect = new DropShadowEffect
+                    {
+                        ShadowDepth = 5,
+                        Direction = -90,
+                        BlurRadius = 20,
+                        Color = Colors.Gray,
+                    };
+
+                    switch (task.Category)
+                    {
+                        case "Дом":
+                            item.TitleTask.Foreground = Brushes.Green;
+                            break;
+                        case "Работа":
+                            item.TitleTask.Foreground = Brushes.Orange;
+                            break;
+                        case "Учеба":
+                            item.TitleTask.Foreground = Brushes.Blue;
+                            break;
+                        case "Отдых":
+                            item.TitleTask.Foreground = Brushes.Purple;
+                            break;
+                        default:
+                            item.TitleTask.Foreground = Brushes.Gray;
+                            break;
+                    }
+
+                    item.MouseDown += TaskItem_MouseDown;
+                    ItemsTasksSPanel.Children.Add(item);
+                }
+            }
+        }
+
+        private void TaskItem_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var taskItem = (TaskItem)sender;
+            if (taskItem == null || taskItem.Task == null) return;
+
+            var item = taskItem.Task;
+
+            TaskI.Visibility = Visibility.Visible;
+            var itemInfo = new TaskInfo();
+            itemInfo.InfoLoad(item);
+
+            TaskInfoBlock.Children.Clear();
+            TaskInfoBlock.Children.Add(itemInfo);
+            itemInfo.DeleteTaskItem += (deletedTask) =>
+            {
+                TaskInfoBlock.Children.Clear();
+                userTasks.Remove(deletedTask);
+                InitializeTasks(userTasks);
+                InitializeCategories();
+            };
+        }
+
+        private void ProfileImageSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                Filter = "Image Files (*.png;*.jpg)| *.png;*.jpg",
+                Title = "Выберите изображение профиля"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+                profileImageButton.Source = bitmap;
+            }
+        }
+
+        private void ProfileImageButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            menuGrid.Visibility = Visibility.Visible;
+        }
+
+        private void MenuGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            menuGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void ExitB_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new LogIn());
+        }
+
+        private void CreateNewTaskB_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new CreateNewTask(userName, userTasks));
+        }
+
+        private void TasksTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            showCompletedTasks = false;
+            selectedCategory = "All";
+            InitializeTasks(userTasks);
+        }
+
+        private void HistoryTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            showCompletedTasks = true;
+            selectedCategory = "All";
+            InitializeTasks(userTasks);
+        }
+
+        private void CategoryTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                var categoryTextBlock = (TextBlock)sender;
+                var categoryName = categoryTextBlock.Text;
+
+                var contextMenu = new ContextMenu();
+
+                var deleteMenuItem = new MenuItem { Header = "Удалить категорию" };
+                deleteMenuItem.Click += (s, args) => DeleteCategory(categoryName);
+
+                if (categoryName == "Дом" || categoryName == "Отдых" || categoryName == "Работа" || categoryName == "Учеба")
+                {
+                    deleteMenuItem.IsEnabled = false;
+                }
+
+                contextMenu.Items.Add(deleteMenuItem);
+
+                categoryTextBlock.ContextMenu = contextMenu;
+                contextMenu.IsOpen = true;
+            }
+            else
+            {
+                showCompletedTasks = false;
+                selectedCategory = ((TextBlock)sender).Text;
+                InitializeTasks(userTasks);
+            }
+        }
+
+        private void DeleteCategory(string categoryName)
+        {
+            userTasks.RemoveAll(task => task.Category == categoryName);
+            InitializeTasks(userTasks);
+
+            var textBlockToRemove = CategorySPanel.Children.Cast<TextBlock>().FirstOrDefault(tb => tb.Text == categoryName);
+            if (textBlockToRemove != null)
+            {
+                CategorySPanel.Children.Remove(textBlockToRemove);
+            }
+
+            MessageBox.Show("Категория удалена успешно!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void RefreshTasks()
+        {
+            InitializeTasks(userTasks);
+        }
+    }
+}
---
+++ View/Registration.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Desktop.Repository;
+
+namespace Desktop.View
+{
+    public partial class Registration : Page
+    {
+        public Registration()
+        {
+            InitializeComponent();
+
+            usernameBox.Foreground = Brushes.Gray;
+            mailBox.Foreground = Brushes.Gray;
+            passBlock.Foreground = Brushes.Gray;
+            pass2Block.Foreground = Brushes.Gray;
+        }
+
+        private void UsernameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (usernameBox.Text == "Введите имя пользователя" && usernameBox.Foreground == Brushes.Gray)
+            {
+                usernameBox.Text = "";
+                usernameBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void UsernameBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (usernameBox.Text == "")
+            {
+                usernameBox.Foreground = Brushes.Gray;
+                usernameBox.Text = "Введите имя пользователя";
+            }
+        }
+
+        private void MailBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (mailBox.Text == "exam@yandex.ru" && mailBox.Foreground == Brushes.Gray)
+            {
+                mailBox.Text = "";
+                mailBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void MailBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (mailBox.Text == "")
+            {
+                mailBox.Foreground = Brushes.Gray;
+                mailBox.Text = "exam@yandex.ru";
+            }
+        }
+
+        private void PassPASSwordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (passPASSwordBox.Password == "")
+                passBlock.Visibility = Visibility.Hidden;
+        }
+
+        private void PassPASSwordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (passPASSwordBox.Password == "")
+                passBlock.Visibility = Visibility.Visible;
+        }
+
+        private void PassPASSwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (passPASSwordBox.Password != "")
+                passBlock.Visibility = Visibility.Hidden;
+            else
+                passBlock.Visibility = Visibility.Visible;
+        }
+
+        private void Pass2PASSwordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (pass2PASSwordBox.Password == "")
+                pass2Block.Visibility = Visibility.Hidden;
+        }
+
+        private void Pass2PASSwordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (pass2PASSwordBox.Password == "")
+                pass2Block.Visibility = Visibility.Visible;
+        }
+
+        private void Pass2PASSwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (pass2PASSwordBox.Password != "")
+                pass2Block.Visibility = Visibility.Hidden;
+            else
+                pass2Block.Visibility = Visibility.Visible;
+        }
+
+        private void NazadB_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new LogIn());
+        }
+
+        private void RegistrationB_Click(object sender, RoutedEventArgs e)
+        {
+            InputValidator inputValidator = new();
+            string mail = mailBox.Text;
+            string name = usernameBox.Text;
+            string password = passPASSwordBox.Password;
+
+            inputValidator.IsValidEmail(mail);
+            inputValidator.IsValidName(name);
+            inputValidator.IsValidPassword(password);
+
+            if (!inputValidator.IsValidName(name) || name == "Введите имя пользователя")
+            {
+                MessageBox.Show("Имя пользователя слишком короткое!\nИмя пользователя обязательно должно содержать не менее трех символов.", "Ошибка регистрации [Имя пользователя]", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (!inputValidator.IsValidEmail(mail) || mailBox.Foreground == Brushes.Gray && mailBox.Text == "exam@yandex.ru")
+                MessageBox.Show("Указан неверный формат Почты!\nПример правильной Почты: example@mail.ru", "Ошибка регистрации [Почта]", MessageBoxButton.OK, MessageBoxImage.Error);
+            else if (!inputValidator.IsValidPassword(password))
+                MessageBox.Show("Слишком короткий Пароль!\nПароль обязательно должен содержать не менее шести символов.", "Ошибка регистрации [Пароль]", MessageBoxButton.OK, MessageBoxImage.Error);
+            else if (password != pass2PASSwordBox.Password)
+                MessageBox.Show("Пароли должны совпадать!", "Ошибка регистрации [Пароль]", MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+            {
+                if (UserRepository.RegisterUser(name, mail, password))
+                {
+                    this.NavigationService.Navigate(new MainEmpty(name));
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь с таким именем или почтой уже существует.", "Ошибка регистрации", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
