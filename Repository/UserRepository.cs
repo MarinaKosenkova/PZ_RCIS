ec4de27c7b1544e538476f6deb8dd5239e4eb601
MarinaKosenkova <koff.06@mail.ru>
2025-02-10 19:00:13 +0300

    Add files via upload

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desktop.Repository
+{
+    public class UserRepository
+    {
+        private static List<UserModel> userModels = new List<UserModel>
+        {
+            new UserModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "root",
+                Email = "root@mail.ru",
+                Password = "rootpassword75"
+            }
+        };
+
+        public static bool RegisterUser(string name, string mail, string password)
+        {
+            if (userModels.Any(use => use.Name == name || use.Email == mail))
+            {
+                return false;
+            }
+
+            var newUserModel = new UserModel
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Email = mail,
+                Password = password
+            };
+
+            userModels.Add(newUserModel);
+            return true;
+        }
+
+        public static UserModel? AuthorizeUser(string mail, string password) =>
+            userModels.FirstOrDefault(use => use.Email == mail && use.Password == password);
+
+        public static UserModel? GetUserByName(string name) =>
+            userModels.FirstOrDefault(user => user.Name == name);
+    }
+}
